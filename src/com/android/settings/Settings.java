5ff881e4f5c561787e8235835ef2e5f86e1a228d
Danny Baumann <dannybaumann@web.de>
2013-11-08 08:55:27 +0100

    Fix calling blacklist settings from other apps.
    Change-Id: I47b737a4c453c1a0ca5b57fc6b58cbd8e0099ac8

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -357,7 +357,8 @@ public class Settings extends PreferenceActivity
         PrintJobSettingsFragment.class.getName(),
         TrustedCredentialsSettings.class.getName(),
         PaymentSettings.class.getName(),
-        KeyboardLayoutPickerFragment.class.getName()
+        KeyboardLayoutPickerFragment.class.getName(),
+        BlacklistSettings.class.getName(),
     };
 
     @Override
@@ -1147,4 +1148,5 @@ public class Settings extends PreferenceActivity
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
     public static class PerformanceSettingsActivity extends Settings { /* empty */ }
+    public static class BlacklistSettingsActivity extends Settings { /* empty */ }
 }
